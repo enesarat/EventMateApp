64d02c59d44dc7339a4378d9f062122603fccdcb
Enes Arat <enes_arat@outlook.com>
2023-05-06 00:08:13 +0300

    Initial commit. Contains:Created core, repository and service layers with Web and API modules.

---
+++ EventMate.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using EventMate.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EventMate.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EventMate.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EventMate.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
