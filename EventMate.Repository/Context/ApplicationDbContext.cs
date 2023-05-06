45047221533aba51038f2ecf03623269c4025ee6
Enes Arat <enes_arat@outlook.com>
2023-05-06 15:41:46 +0300

    Contains: A database context class has been created in the repository layer.

---
+++ EventMate.Repository/Context/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using EventMate.Core.Model.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMate.Repository.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get;set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Ticket> Ticket { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            foreach (var relationship in builder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
