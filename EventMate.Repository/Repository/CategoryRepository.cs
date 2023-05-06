60cfa0b12fad77e7be09a4b80b60599810609d2b
Enes Arat <enes_arat@outlook.com>
2023-05-06 17:32:28 +0300

    Contains: Repository classes have been created for Generic Repository and other entities.

---
+++ EventMate.Repository/Repository/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using EventMate.Core.Model.Concrete;
+using EventMate.Core.Repository;
+using EventMate.Repository.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMate.Repository.Repository
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ EventMate.Repository/Repository/GenericRepository.cs
@@ -0,0 +1,66 @@
+﻿using EventMate.Core.Repository;
+using EventMate.Repository.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMate.Repository.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _entities;
+
+        public GenericRepository(ApplicationDbContext dbContext)
+        {
+            _context = dbContext;
+            _entities = _context.Set<T>();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _entities.AddAsync(entity);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+            return await _entities.AnyAsync(expression);
+        }
+
+        public void Delete(T entity)
+        {
+            var column = entity.GetType().GetProperty("IsActive");
+            if (column is not null)
+            {
+                entity.GetType().GetProperty("IsActive").SetValue(entity, false);
+            }
+            else
+            {
+                _entities.Remove(entity);
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _entities.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _entities.FindAsync(id);
+        }
+
+        public void Update(T entity)
+        {
+            _entities.Update(entity);
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            return _entities.Where(expression);
+        }
+    }
+}
