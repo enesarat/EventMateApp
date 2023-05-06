b4f53a917df4427bc4c4e1d1a1a4f98f2056be52
Enes Arat <enes_arat@outlook.com>
2023-05-07 00:17:01 +0300

    Contains: Custom response classes have been created for use in endpoints.

---
+++ EventMate.Core/DTO/Concrete/Response/NoContentResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventMate.Core.DTO.Concrete.Response
+{
+    public class NoContentResponse
+    {
+    }
+}
